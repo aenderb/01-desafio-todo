cc41ddf0160f258f67fda2650ac50fb0e7081054
Aender Binoto <aenderbinoto@MacBook-Pro-de-Aender.local>
2023-04-10 16:11:04 -0300

    fix ListStatus

--- src/components/TaskListStatus.tsx
+++ src/components/TaskListStatus.tsx
@@ -5,41 +5,21 @@ import styles from './TaskListStatus.module.css'
 export function TaskListStatus() {
   const taskCounter = 5
 
-  if (taskCounter !== 0) {
-    return (
-      <div>
-        <div className={styles.taskListStatus}>
-          <header>
-            <div className={styles.taskC}>
-              <span className={styles.taskCreated}>Tarefas Criadas</span>
-              <span className={styles.taskCounter}>{taskCounter}</span>
-            </div>
-            <div className={styles.taskD}>
-              <span className={styles.taskDone}>Tarefas Concluídas</span>
-              <span className={styles.taskCounterDone}>2 de 5</span>
-            </div>
-          </header>
-        </div>
-        <TaskList />
+  return (
+    <div>
+      <div className={styles.taskListStatus}>
+        <header>
+          <div className={styles.taskC}>
+            <span className={styles.taskCreated}>Tarefas Criadas</span>
+            <span className={styles.taskCounter}>{taskCounter}</span>
+          </div>
+          <div className={styles.taskD}>
+            <span className={styles.taskDone}>Concluídas</span>
+            <span className={styles.taskCounterDone}>2 de 5</span>
+          </div>
+        </header>
       </div>
-    )
-  } else {
-    return (
-      <div>
-        <div className={styles.taskListStatus}>
-          <header>
-            <div className={styles.taskC}>
-              <span className={styles.taskCreated}>Tarefas Criadas</span>
-              <span className={styles.taskCounter}>{taskCounter}</span>
-            </div>
-            <div className={styles.taskD}>
-              <span className={styles.taskDone}>Tarefas Concluídas</span>
-              <span className={styles.taskCounterDone}>2 de 5</span>
-            </div>
-          </header>
-        </div>
-        <TaskListEmpty />
-      </div>
-    )
-  }
+      {taskCounter !== 0 ? <TaskList /> : <TaskListEmpty />}
+    </div>
+  )
 }
